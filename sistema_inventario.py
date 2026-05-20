b5bd286b39c5ba538a78954b9a8c10f6aaab913d
dev_paulo <fyptelsac@gmail.com>
2026-05-19 20:26:04 -0500

    Agrega validacion adicional para cantidad negativa

--- sistema_inventario.py
+++ sistema_inventario.py
@@ -1,3 +1,8 @@
 nombre = input("Nombre del producto: ")
 cantidad = int(input("Cantidad: "))
-print("Producto:", nombre, "Cantidad:", cantidad)
+#print("Producto:", nombre, "Cantidad:", cantidad)
+
+if cantidad < 0:
+	print ("la cantidad no puede ser negativa")
+else:
+	print ("Producto: ",nombre, " Cantidad: ",cantidad)
