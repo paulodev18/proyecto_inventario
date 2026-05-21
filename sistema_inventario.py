6dc3f0424c19d85b5d06129ebb64b639e7a40fea
dev_paulo <fyptelsac@gmail.com>
2026-05-21 09:32:31 -0500

    bucle while para agregar producto

--- sistema_inventario.py
+++ sistema_inventario.py
@@ -1,4 +1,13 @@
+inventario = {}
+while True:
+	nombre = input("Nombre del producto o salir: ")
+	if nombre == "salir":
+		break
+	cantidad = int(input("Cantidad: "))
 
-print("=== INVENTARIO ===")
-for producto, cantidad in inventario.items():
-    print(f"{producto}: {cantidad} unidades")
+	if cantidad < 0:
+		print ("cantidad inválida, No se agregó el producto.")
+	else:
+		inventario[nombre] = cantidad
+		print ("Producto agregado.")
+print ("Inventario final:",inventario)
